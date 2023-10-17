0c7e96b5209b702c0a5f23d9278bceaba383c1c4
IanaKos <95440446+IanaKos@users.noreply.github.com>
2023-10-17 09:56:45 +0200

    gallery items were added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const imagesEl = galleryItems.map(
+  (item) => `<div class = "gallery_item"> <a class="gallery_link" </div>`
+);
